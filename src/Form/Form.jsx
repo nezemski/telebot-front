41dba6e28e12a1303ea4271350a3be998db78a5b
nezemski <nezemski423@gmail.com>
2025-10-10 10:40:15 +0300

    55

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -16,9 +16,9 @@ const Form = () => {
   }, [city, street, subject, country]);
 
   useEffect(() => {
-    tg.WebApp.onEvent("mainButtonClicked", onSendData);
+    tg.onEvent("mainButtonClicked", onSendData);
     return () => {
-      tg.WebApp.offEvent("mainButtonClicked", onSendData);
+      tg.offEvent("mainButtonClicked", onSendData);
     };
   }, [onSendData]);
 
